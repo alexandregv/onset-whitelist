5bccea518d124b30b45114b83e63df27880c2242
Alexandre Guiot--Valentin <contact@alexandregv.fr>
2020-01-01 08:00:45 +0100

    Refactor main command

--- server/whitelist.lua
+++ server/whitelist.lua
@@ -1,6 +1,31 @@
 local whitelistIsEnabled = true
 local whitelist = {}
 
+-- utils
+
+function GetPlayerFromPartialName(name)
+	local name = name and name:gsub("#%x%x%x%x%x%x", ""):lower() or nil
+	if name then
+		for _, player in ipairs(GetAllPlayers()) do
+			local playerName = GetPlayerName(player):gsub("#%x%x%x%x%x%x", ""):lower()
+			if playerName:find(name, 1, true) then
+				return player
+			end
+		end
+	end
+end
+
+function GetPlayerFromSteamId(steamid)
+	for _, player in ipairs(GetAllPlayers()) do
+		if GetPlayerSteamId(player) == tonumber(steamid) then
+			return player
+		end
+	end
+	return nil
+end
+
+-- whitelist functions
+
 function getWhitelistLength()
 	local length = 0
 	for _, __ in pairs(whitelist) do
@@ -34,121 +59,138 @@ function saveWhitelist()
 	print("[whitelist] ".."Whitelist saved! ("..(getWhitelistLength()).." entries)")
 end
 
-function GetPlayerFromPartialName(name)
-	local name = name and name:gsub("#%x%x%x%x%x%x", ""):lower() or nil
-	if name then
-		for _, player in ipairs(GetAllPlayers()) do
-			local playerName = GetPlayerName(player):gsub("#%x%x%x%x%x%x", ""):lower()
-			if playerName:find(name, 1, true) then
-				return player
-			end
-		end
+-- subcommands
+
+function list(player, arg)
+	AddPlayerChat(player, "[whitelist] ".."Whitelist:")
+	for k, _ in pairs(whitelist) do
+		local p = GetPlayerFromSteamId(k)
+		AddPlayerChat(player, "[whitelist] ".." - "..k..(p and ' ('..GetPlayerName(p)..')' or ''))
+	end
+	AddPlayerChat(player, "[whitelist] ".."--------------------")
+end
+
+function reload(player)
+	loadWhitelist(player)
+end
+
+function add(player, arg, ...)
+	if #{...} > 0 then
+		arg = arg.." "..table.concat({...}, " ")
+	end
+
+	if arg == nil then
+		AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist add <name|steamid>")
+		return
+	end
+
+	local target = nil
+	if arg:match("^%d+$") ~= nil then
+		AddPlayerChat(player, "ID "..arg)
+		target = GetPlayerFromSteamId(arg)
+	else
+		target = GetPlayerFromPartialName(arg)
+	end
+
+	if target == nil then
+		AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
+	else
+			local steamid = GetPlayerSteamId(target)
+			whitelist[steamid] = 1
+			AddPlayerChat(player, "[whitelist] "..steamid.." ("..GetPlayerName(target)..") has been added to the whitelist")
+			saveWhitelist()
 	end
 end
 
-function GetPlayerFromSteamId(steamid)
-	for _, player in ipairs(GetAllPlayers()) do
-		if GetPlayerSteamId(player) == tonumber(steamid) then
-			return player
-		end
+function remove(player, arg, ...)
+	if #{...} > 0 then
+		arg = arg.." "..table.concat({...}, " ")
 	end
-	return nil
+
+	if arg == nil then
+		AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist remove <name|steamid>")
+		return
+	end
+
+	local target = nil
+	if arg:match("^%d+$") ~= nil then
+		AddPlayerChat(player, "ID "..arg)
+		target = GetPlayerFromSteamId(arg)
+	else
+		target = GetPlayerFromPartialName(arg)
+	end
+
+	if target == nil then
+		AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
+	else
+			local steamid = GetPlayerSteamId(target)
+			whitelist[steamid] = nil
+			AddPlayerChat(player, "[whitelist] "..steamid.." ("..GetPlayerName(target)..") has been removed from the whitelist")
+			saveWhitelist()
+	end
 end
 
-function main_command(player, subcmd, arg, ...)
--- list
-	if subcmd == nil or subcmd == "list" then
-		AddPlayerChat(player, "[whitelist] ".."Whitelist:")
-		for k, _ in pairs(whitelist) do
-			local p = GetPlayerFromSteamId(k)
-			AddPlayerChat(player, "[whitelist] ".." - "..k..(p and ' ('..GetPlayerName(p)..')' or ''))
-		end
-		AddPlayerChat(player, "[whitelist] ".."--------------------")
--- reload
-	elseif subcmd == "reload" then
-		loadWhitelist(player)
--- add
-	elseif subcmd == "add" or subcmd == "+" then
-		if #{...} > 0 then
-			arg = arg.." "..table.concat({...}, " ")
-		end
+function check(player, arg, ...)
+	if #{...} > 0 then
+		arg = arg.." "..table.concat({...}, " ")
+	end
 
-		if arg == nil then
-			AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist add <name|steamid>")
-			return
-		end
+	if arg == nil then
+		AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist check <name|steamid>")
+		return
+	end
 
-		local target = nil
-		if arg:match("^%d+$") ~= nil then
-			AddPlayerChat(player, "ID "..arg)
-			target = GetPlayerFromSteamId(arg)
-		else
-			target = GetPlayerFromPartialName(arg)
-		end
+	local target = nil
+	if arg:match("^%d+$") ~= nil then
+		AddPlayerChat(player, "ID "..arg)
+		target = GetPlayerFromSteamId(arg)
+	else
+		target = GetPlayerFromPartialName(arg)
+	end
+	if target == nil then
+		AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
+	else
+			AddPlayerChat(player, "[whitelist] "..GetPlayerSteamId(target).." ("..GetPlayerName(target)..") is "..(whitelist[GetPlayerSteamId(target)] == 1 and '' or 'NOT ').."whitelisted")
+	end
+end
 
-		if target == nil then
-			AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
-		else
-				local steamid = GetPlayerSteamId(target)
-				whitelist[steamid] = 1
-				AddPlayerChat(player, "[whitelist] "..steamid.." ("..GetPlayerName(target)..") has been added to the whitelist")
-				saveWhitelist()
-		end
--- remove
-	elseif subcmd == "remove" or subcmd == "-" then
-		if #{...} > 0 then
-			arg = arg.." "..table.concat({...}, " ")
-		end
+-- main command
 
-		if arg == nil then
-			AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist remove <name|steamid>")
-			return
-		end
+local cmds = {
+	["help"] = help,
+	["h"] = help,
 
-		local target = nil
-		if arg:match("^%d+$") ~= nil then
-			AddPlayerChat(player, "ID "..arg)
-			target = GetPlayerFromSteamId(arg)
-		else
-			target = GetPlayerFromPartialName(arg)
-		end
+	["list"] = list,
+	["l"] = list,
 
-		if target == nil then
-			AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
-		else
-				local steamid = GetPlayerSteamId(target)
-				whitelist[steamid] = nil
-				AddPlayerChat(player, "[whitelist] "..steamid.." ("..GetPlayerName(target)..") has been removed from the whitelist")
-				saveWhitelist()
-		end
--- check
-	elseif subcmd == "check" then
-		if #{...} > 0 then
-			arg = arg.." "..table.concat({...}, " ")
-		end
+	["add"] = add,
+	["+"] = add,
 
-		if arg == nil then
-			AddPlayerChat(player, "[whitelist] ".."Usage: /whitelist check <name|steamid>")
-			return
-		end
+	["remove"] = remove,
+	["-"] = remove,
 
-		local target = nil
-		if arg:match("^%d+$") ~= nil then
-			AddPlayerChat(player, "ID "..arg)
-			target = GetPlayerFromSteamId(arg)
-		else
-			target = GetPlayerFromPartialName(arg)
-		end
-		if target == nil then
-			AddPlayerChat(player, "[whitelist] ".."Can't find player: "..arg)
-		else
-				AddPlayerChat(player, "[whitelist] "..GetPlayerSteamId(target).." ("..GetPlayerName(target)..") is "..(whitelist[GetPlayerSteamId(target)] == 1 and '' or 'NOT ').."whitelisted")
-		end
+	["check"] = check,
+	["c"] = check,
+
+	["reload"] = reload,
+	["r"] = reload,
+}
+
+function main_command(player, cmd, arg, ...)
+	local cmdfunc = cmds[cmd]
+	if cmd == nil then
+		cmds["help"](player)
+	elseif cmdfunc then
+		cmdfunc(player, arg, ...)
+	else
+		cmds["help"](player)
 	end
 end
 
 AddCommand("whitelist", main_command)
 AddCommand("wl", main_command)
+
+-- events
 
 AddEvent("OnPlayerSteamAuth", function(player)
 	local steamid = GetPlayerSteamId(player)
